2467bcfd2802ad0b088f6e83412c56b8252a8b10
pabla <kanin.pavel@gmail.com>
2018-12-06 18:50:32 +0700

    Make source of layout configurable

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+const { URL } = require('url');
 const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
@@ -68,6 +69,15 @@ const fixLinks = $ => {
     });
 };
 
+const fixRoot = $ => {
+    $('#theme-page')
+        .empty()
+        .removeAttr('class')
+        .removeAttr('role')
+        .removeAttr('itemprop')
+        .attr('id', 'root');
+};
+
 const addPlaceholders = ($, placeholders = {}) => {
     if (placeholders.title) {
         $('head').append(`<title>${placeholders.title}</title>`);
@@ -88,60 +98,72 @@ const addPlaceholders = ($, placeholders = {}) => {
             $('body').append(placeholders.body.append);
         }
     }
-
-    // add root node
-    const node = $('#theme-page')
-        .empty()
-        .removeAttr('class')
-        .removeAttr('role')
-        .removeAttr('itemprop');
     if (placeholders.root) {
-        node.append(placeholders.root);
-    } else {
-        node.attr('id', 'root');
+        $('#root').append(placeholders.root);
     }
 };
 
-const collectFiles = async ($, publicDirectory) => {
-    const files = new Set();
-    const createFileCollector = attr => (i, node) => {
+const applyToSources = async ($, fn) => {
+    const lazyFns = [];
+    const collect = attr => (...args) => {
+        lazyFns.push(() => fn(attr)(...args));
+    };
+    $('img[data-cfsrc*="/wp-content/"]').each(collect('data-cfsrc'));
+    $('link[href*="/wp-content/"]').each(collect('href'));
+    $('script[src*="/wp-content/"]').each(collect('src'));
+    $('script[src*="/wp-includes/"]').each(collect('src'));
+    for (let lazyFn of lazyFns) {
+        await lazyFn();
+    }
+};
+
+const fixSources = async ($, selectors) => {
+    const fn = attr => (i, node) => {
         let src = node.attribs[attr];
         if (src.startsWith('//')) {
             src = `https:${src}`;
         }
         node.attribs[attr] = src.replace(/(https)?:\/\/www.plesk.com\//, '/');
-        files.add(src);
     };
-    $('img[data-cfsrc^="https://www.plesk.com/wp-content/"]').each(createFileCollector('data-cfsrc'));
-    $('link[href*="//www.plesk.com/wp-content/"]').each(createFileCollector('href'));
-    $('script[src*="//www.plesk.com/wp-content/"]').each(createFileCollector('src'));
-    $('script[src*="//www.plesk.com/wp-includes/"]').each(createFileCollector('src'));
+    await applyToSources($, fn);
+};
 
-    for (const src of files) {
-        const dst = path.resolve(publicDirectory, src.replace('https://www.plesk.com/', ''));
+const collectFiles = async ($, { publicDirectory, origin }) => {
+    const fn = attr => async (i, node) => {
+        let src = node.attribs[attr];
+        const dst = path.resolve(publicDirectory, src.replace(/^\//, ''));
         if (!dst.startsWith(path.resolve(publicDirectory))) {
             throw new Error(`The source url "${src}" is invalid`);
         }
         if (!fs.existsSync(dst)) {
             await fse.ensureDir(path.dirname(dst));
-            await downloadFile(src, dst);
+            await downloadFile(origin + src, dst);
         }
-    }
+    };
+    await applyToSources($, fn);
 };
 
-const downloadLayout = async ({ filename, publicDirectory, placeholders, minify = false, modify } = {}) => {
+const downloadLayout = async ({ url = 'https://www.plesk.com/extensions/', filename, publicDirectory, placeholders, minify = false, modify } = {}) => {
     if (!publicDirectory) {
         throw new Error('The "publicDirectory" option is required');
     }
-    let html = await getHTML('https://www.plesk.com/extensions/');
+
+    let html = await getHTML(url);
     const $ = cheerio.load(html, { decodeEntities: false });
 
-    removeUselessNodes($);
-    removeHighlightFromMenu($);
-    fixLinks($);
+    const { origin } = new URL(url);
+
+    if (origin === 'https://www.plesk.com') {
+        removeUselessNodes($);
+        removeHighlightFromMenu($);
+        fixLinks($);
+        fixSources($);
+        fixRoot($);
+    }
+
     addPlaceholders($, placeholders);
 
-    await collectFiles($, publicDirectory);
+    await collectFiles($, { publicDirectory, origin });
 
     if (typeof modify === 'function') {
         modify($);
